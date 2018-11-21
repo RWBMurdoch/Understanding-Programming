0211fe0db9e112d2559f55e9b91b4088d75eb1fb
user <user@users-MacBook-Pro-5.local>
2018-11-21 15:27:38 +0000

    finish the app

--- countries_app/main.rb
+++ countries_app/main.rb
@@ -1,23 +1,30 @@
 require_relative('./country_functions')
 require('json')
+require('net/http')
+require('sinatra')
 
-countries_file = File.read('countries.json')
-countries = JSON.parse(countries_file)
+uri = URI ('https://restcountries.eu/rest/v2/all')
+countries_json = Net::HTTP.get(uri)
+COUNTRIES = JSON.parse(countries_json)
 
-country_name = name_of_first_country(countries)
-puts country_name
+get '/home' do
+  erb( :home_view )
+end
 
-population = population_of_first_country(countries)
-puts population
-
-world_population = population_of_all_countries(countries)
-puts world_population
-
-european = number_of_countries_in_europe(countries)
-puts european
-
-asia = population_of_asia(countries)
-puts asia
-
-africa = population_of_africa(countries)
-puts africa 
+# country_name = name_of_first_country(countries)
+# puts country_name
+#
+# population = population_of_first_country(countries)
+# puts population
+#
+# world_population = population_of_all_countries(countries)
+# puts world_population
+#
+# european = number_of_countries_in_europe(countries)
+# puts european
+#
+# asia = population_of_asia(countries)
+# puts asia
+#
+# africa = population_of_africa(countries)
+# puts africa
